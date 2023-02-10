a2b2dbc8ac4da87d03e36b86b969014c1ad245b6
Jprogrammer13 <buss.riky@gmail.com>
2023-02-10 14:36:31 +0100

    fixed for no-detections

--- re-id.py
+++ re-id.py
@@ -108,23 +108,37 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
             # get results and convert to numpy
             results = model.predict(frame_copy, verbose=False)[0].cpu().numpy()
 
-            # filter prediction of class person with conf > 0.5
-            idx = np.where((results.boxes.cls == 0) & (
-                results.boxes.conf > 0.6))  # filter person
+            if len(results) > 0: #if someone detected 
 
-            # extract masks,boxes and bb centers
-            masks = results.masks.masks[idx].copy()
-            boxes = results.boxes.xyxy[idx].copy()
-            centers = results.boxes.xywh[idx][:, :2].copy().astype(int)
+                # filter prediction of class person with conf > 0.5
+                idx = np.where((results.boxes.cls == 0) & (
+                    results.boxes.conf > 0.6))  # filter person
 
-            # populate detection array with associeted detections
-            buffer_detections[frame_id] = pd.DataFrame({
-                'box': boxes.tolist(),
-                'mask': masks.tolist(),
-                'center': centers.tolist(),
-                'box_id': np.full(len(idx[0]), -1).tolist(),
-                'track_id': np.full(len(idx[0]), -1).tolist()
-            }).copy()
+                #TODO: implement filter of box ratio (no person)
+                # print(results.boxes.xywh)
+
+                # extract masks,boxes and bb centers
+                masks = results.masks.masks[idx].copy()
+                boxes = results.boxes.xyxy[idx].copy()
+                centers = results.boxes.xywh[idx][:, :2].copy().astype(int)
+
+                # populate detection array with associeted detections
+                buffer_detections[frame_id] = pd.DataFrame({
+                    'box': boxes.tolist(),
+                    'mask': masks.tolist(),
+                    'center': centers.tolist(),
+                    'box_id': np.full(len(idx[0]), -1).tolist(),
+                    'track_id': np.full(len(idx[0]), -1).tolist()
+                }).copy()
+            else:
+                # populate detection array with empty detections
+                buffer_detections[frame_id] = pd.DataFrame({
+                    'box': [],
+                    'mask': [],
+                    'center': [],
+                    'box_id': [],
+                    'track_id': []
+                }).copy()
 
             # delete yolo drawed frame
             del frame_copy, results
@@ -139,106 +153,118 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
         for j in buffer_detections:
             for val in j['center'].values:
                 centers.append(val)
+        
+        if len(centers) > 0: # if someone detected 
 
-        # fit kmeans cluster with n_cluster equal to max detections per frame
-        kmeans = KMeans(n_clusters=cluster_numbers,
-                        random_state=0, n_init="auto").fit(centers)
+            # fit kmeans cluster with n_cluster equal to max detections per frame
+            kmeans = KMeans(n_clusters=cluster_numbers,
+                            random_state=0, n_init="auto").fit(centers)
 
-        # for each detections find the box number
-        for det in buffer_detections:
-            det['box_id'] = det['center'].apply(
-                lambda x: kmeans.predict(np.array([x]).tolist())[0])
+            # for each detections find the box number
+            for det in buffer_detections:
+                det['box_id'] = det['center'].apply(
+                    lambda x: kmeans.predict(np.array([x]).tolist())[0])
 
-        del cluster_numbers, centers, kmeans
+            del cluster_numbers, centers, kmeans
 
-        # for each detection calculate feature vector and the correspondent track_id [-1 if no similarity found]
-        for i, det in enumerate(buffer_detections):
-            # in case of propagated -1 recalculate the track_id else keep the propagated
-            det['track_id'] = det.apply(lambda x: get_tid(track_id_df, Image.fromarray(crop_segmentation(np.array(
-                x['mask']), np.array(x['box']), buffer_frames[i]))) if int(x['track_id']) == -1 else x['track_id'], axis=1)
-            if (i >= buffer_idx):  # calculate fv only for the missing one
-                det['fv'] = det.apply(lambda x: siamese_net.fv_encoding(Image.fromarray(crop_segmentation(
-                    np.array(x['mask']), np.array(x['box']), buffer_frames[i]))).cpu().numpy(), axis=1)
+            # for each detection calculate feature vector and the correspondent track_id [-1 if no similarity found]
+            for i, det in enumerate(buffer_detections):
+                # in case of propagated -1 recalculate the track_id else keep the propagated
+                if len(det) > 0:  # if someone detected 
+                    det['track_id'] = det.apply(lambda x: get_tid(track_id_df, Image.fromarray(crop_segmentation(np.array(
+                        x['mask']), np.array(x['box']), buffer_frames[i]))) if int(x['track_id']) == -1 else x['track_id'], axis=1)
+                    if (i >= buffer_idx):  # calculate fv only for the missing one
+                        det['fv'] = det.apply(lambda x: siamese_net.fv_encoding(Image.fromarray(crop_segmentation(
+                            np.array(x['mask']), np.array(x['box']), buffer_frames[i]))).cpu().numpy(), axis=1)
 
-        # for each detection id get the four tid prediction
-        counter = {}
+            # for each detection id get the four tid prediction
+            counter = {}
 
-        for i, det in enumerate(buffer_detections):
-            for index, row in det.iterrows():
-                tmp_box_id = row['box_id']
-                if str(tmp_box_id) not in counter:
-                    counter[str(tmp_box_id)] = [row['track_id']]
-                else:
-                    counter[str(tmp_box_id)].append(row['track_id'])
-
-        # for each detection count the occurence of tid associeted, if < 3 return None (not valid)
-        for key in counter:
-            # counter[key] = max(counter[key],key=counter[key].count)
-            counting_inst = {str(u): counter[key].count(
-                u) for u in np.unique(np.array(counter[key]))}
-            candidate_tid = max(counting_inst, key=counting_inst.get)
-            tid = candidate_tid if counting_inst[candidate_tid] >= (
-                frames_window//2) else None
-            counter[key] = tid
-
-        used = []
-        # implement filter on duplicate tid
-        for id in counter:
-            if counter[id] is not None:  # if None don't evaluate
-                if int(counter[id]) != -1:  # ignore -1 (to add)
-                    if counter[id] not in used:
-                        used.append(counter[id])
-                    else:
-                        counter[id] = None
-
-        # for each detection, set the processed tid
-        for det in buffer_detections:
-            det['track_id'] = det['box_id'].apply(lambda x: counter[str(x)])
-
-        del counter
-        del used
-
-        added = {}
-        for det in buffer_detections:
-            for index, row in det.iterrows():
-                if row['track_id'] is not None:
-                    if int(row['track_id']) == -1:
-                        if row['box_id'] not in added:
-                            # add fv to track_id and return last index
-                            color = np.random.randint(0, 255, 3).tolist()
-                            track_id_df = pd.concat([track_id_df, pd.DataFrame(
-                                {"fv": [row['fv']], 'color':[color]})], ignore_index=True)
-                            new_tid = track_id_df.index[-1]
-                            added[row['box_id']] = new_tid
-                            det._set_value(index, 'track_id', new_tid)
-                            row['track_id'] = new_tid
-                            # print(f"aggiunto {row['box_id']}")
+            for i, det in enumerate(buffer_detections):
+                if len(det) > 0:  # if someone detected 
+                    for index, row in det.iterrows():
+                        tmp_box_id = row['box_id']
+                        if str(tmp_box_id) not in counter:
+                            counter[str(tmp_box_id)] = [row['track_id']]
                         else:
-                            det._set_value(index, 'track_id',
-                                           added[row['box_id']])
-                            row['track_id'] = added[row['box_id']]
+                            counter[str(tmp_box_id)].append(row['track_id'])
 
-                    # track_id_df.loc[int(row['track_id'])]['fv'] = row['fv']
+            # for each detection count the occurence of tid associeted, if < 3 return None (not valid)
+            for key in counter:
+                # counter[key] = max(counter[key],key=counter[key].count)
+                counting_inst = {str(u): counter[key].count(
+                    u) for u in np.unique(np.array(counter[key]))}
+                candidate_tid = max(counting_inst, key=counting_inst.get)
+                tid = candidate_tid if counting_inst[candidate_tid] >= (
+                    frames_window//2) else None
+                counter[key] = tid
 
-        del added
+            used = []
+            # implement filter on duplicate tid
+            for id in counter:
+                if counter[id] is not None:  # if None don't evaluate
+                    if int(counter[id]) != -1:  # ignore -1 (to add)
+                        if counter[id] not in used:
+                            used.append(counter[id])
+                        else:
+                            counter[id] = None
 
-        for f in range(frames_window//2):
-            drew_frame = buffer_frames[f].copy()
-            for indx, row in buffer_detections[f].iterrows():
-                if row['track_id'] is not None:
-                    color = track_id_df.loc[int(row['track_id'])]['color']
-                    drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
-                        row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 3)
-                    drew_frame = cv2.putText(drew_frame, str(row['track_id']), (int(row['box'][0]), int(
-                        row['box'][1])), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2, cv2.LINE_AA)
-                else:
-                    color = (255, 255, 255)
-                    drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
-                        row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 1)
+            # for each detection, set the processed tid
+            for det in buffer_detections:
+                det['track_id'] = det['box_id'].apply(lambda x: counter[str(x)])
 
-            # cv2.imwrite(f"./prova/{time.time()}.jpg", drew_frame)
-            video_out.write(drew_frame.copy())
-            del drew_frame
+            del counter
+            del used
+
+            added = {}
+            for det in buffer_detections:
+                if len(det) > 0:  # if someone detected 
+                    for index, row in det.iterrows():
+                        if row['track_id'] is not None:
+                            if int(row['track_id']) == -1:
+                                if row['box_id'] not in added:
+                                    # add fv to track_id and return last index
+                                    color = np.random.randint(0, 255, 3).tolist()
+                                    track_id_df = pd.concat([track_id_df, pd.DataFrame(
+                                        {"fv": [row['fv']], 'color':[color]})], ignore_index=True)
+                                    new_tid = track_id_df.index[-1]
+                                    added[row['box_id']] = new_tid
+                                    det._set_value(index, 'track_id', new_tid)
+                                    row['track_id'] = new_tid
+                                    # print(f"aggiunto {row['box_id']}")
+                                else:
+                                    det._set_value(index, 'track_id',
+                                                added[row['box_id']])
+                                    row['track_id'] = added[row['box_id']]
+
+                            # track_id_df.loc[int(row['track_id'])]['fv'] = row['fv']
+
+            del added
+
+            for f in range(frames_window//2):
+                drew_frame = buffer_frames[f].copy()
+                for indx, row in buffer_detections[f].iterrows():
+                    if row['track_id'] is not None:
+                        color = track_id_df.loc[int(row['track_id'])]['color']
+                        drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
+                            row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 3)
+                        drew_frame = cv2.putText(drew_frame, str(row['track_id']), (int(row['box'][0]), int(
+                            row['box'][1])), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2, cv2.LINE_AA)
+                    else:
+                        color = (255, 255, 255)
+                        drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
+                            row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 1)
+
+                # cv2.imwrite(f"./prova/{time.time()}.jpg", drew_frame)
+                video_out.write(drew_frame.copy())
+                del drew_frame
+
+        else:
+            for f in range(frames_window//2):
+                drew_frame = buffer_frames[f].copy()
+                video_out.write(drew_frame.copy())
+                del drew_frame
+
 
         # clean detection processing
         for det in buffer_detections:
