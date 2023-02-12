b92384dd9fc4059a7f5b46ab817e82167bcd779a
Jprogrammer13 <buss.riky@gmail.com>
2023-02-12 17:05:46 +0100

    added mars training and refactor of re-id.py

--- re-id.py
+++ re-id.py
@@ -20,16 +20,20 @@ import progressbar
 # COCO128 classes https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
 model = YOLO("yolov8m-seg.pt")
 # model = YOLO("yolov8n-seg.pt")
-siamese_net = SiameseReId(os.path.join('model', 'weights', 'model_final.pt'))
+# siamese_net = SiameseReId(os.path.join('model', 'weights', 'model_final_default.pt'))
+siamese_net = SiameseReId(os.path.join('model', 'weights', 'model_final_mars.pt'))
 
+# function to get cropped image of the bb size with segmentation mask applied
+def crop_segmentation(_mask, _box, _image):
+    img = _image.copy()
+    box = _box.astype(int)
 
-def crop_segmentation(segment, box, image):
-    img = image.copy()
-    box = box.astype(int)
-    w, h, c = img.shape
-    mask = (cv2.resize(segment, (h, w)) > 0).astype("uint8")
+    w, h, _ = img.shape
+    mask = (cv2.resize(_mask, (h, w)) > 0).astype("uint8")
+
     img_segm = cv2.bitwise_and(img, img, mask=mask)
     img_segm = img_segm[box[1]:box[3], box[0]:box[2]]
+
     return cv2.cvtColor(img_segm, cv2.COLOR_BGR2RGB)
 
 # function to get tid given the detection image [return -1 if no similarity find]
@@ -38,6 +42,7 @@ def crop_segmentation(segment, box, image):
 def get_tid(track_id_df, det_img):
     similarity = track_id_df['fv'].apply(lambda x: siamese_net.similarity(
         det_img, torch.tensor(x)).detach().cpu().numpy()[0][0])
+
     if similarity.empty:
         return -1
     else:
@@ -47,7 +52,9 @@ def get_tid(track_id_df, det_img):
 def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
 
     file_path = _video_in
+
     cap = cv2.VideoCapture(file_path)
+
     video_out = cv2.VideoWriter(_video_out, cv2.VideoWriter_fourcc(*'DIVX'), int(cap.get(
         cv2.CAP_PROP_FPS)), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
@@ -61,6 +68,7 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
     vid_w, vid_h, vid_length = int(cap.get(3)), int(
         cap.get(4)), int(cap.get(7))
 
+    # use total number of frame if -1 is passed
     if total_frame == -1:
         total_frame = vid_length
 
@@ -108,14 +116,11 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
             # get results and convert to numpy
             results = model.predict(frame_copy, verbose=False)[0].cpu().numpy()
 
-            if len(results) > 0: #if someone detected 
+            if len(results) > 0:  # if someone detected
 
                 # filter prediction of class person with conf > 0.5
                 idx = np.where((results.boxes.cls == 0) & (
                     results.boxes.conf > 0.6))  # filter person
-
-                #TODO: implement filter of box ratio (no person)
-                # print(results.boxes.xywh)
 
                 # extract masks,boxes and bb centers
                 masks = results.masks.masks[idx].copy()
@@ -130,7 +135,7 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
                     'box_id': np.full(len(idx[0]), -1).tolist(),
                     'track_id': np.full(len(idx[0]), -1).tolist()
                 }).copy()
-            else:
+            else: # no people detected
                 # populate detection array with empty detections
                 buffer_detections[frame_id] = pd.DataFrame({
                     'box': [],
@@ -150,11 +155,11 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
         # create a list containing all the detections per frame
         centers = []
 
-        for j in buffer_detections:
-            for val in j['center'].values:
+        for det in buffer_detections:
+            for val in det['center'].values:
                 centers.append(val)
-        
-        if len(centers) > 0: # if someone detected 
+
+        if len(centers) > 0:  # if someone detected
 
             # fit kmeans cluster with n_cluster equal to max detections per frame
             kmeans = KMeans(n_clusters=cluster_numbers,
@@ -170,18 +175,18 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
             # for each detection calculate feature vector and the correspondent track_id [-1 if no similarity found]
             for i, det in enumerate(buffer_detections):
                 # in case of propagated -1 recalculate the track_id else keep the propagated
-                if len(det) > 0:  # if someone detected 
+                if len(det) > 0:  # if someone detected
                     det['track_id'] = det.apply(lambda x: get_tid(track_id_df, Image.fromarray(crop_segmentation(np.array(
                         x['mask']), np.array(x['box']), buffer_frames[i]))) if int(x['track_id']) == -1 else x['track_id'], axis=1)
-                    if (i >= buffer_idx):  # calculate fv only for the missing one
-                        det['fv'] = det.apply(lambda x: siamese_net.fv_encoding(Image.fromarray(crop_segmentation(
-                            np.array(x['mask']), np.array(x['box']), buffer_frames[i]))).cpu().numpy(), axis=1)
+                    # if (i >= buffer_idx):  # calculate fv only for the missing one
+                    #     det['fv'] = det.apply(lambda x: siamese_net.fv_encoding(Image.fromarray(crop_segmentation(
+                    #         np.array(x['mask']), np.array(x['box']), buffer_frames[i]))).cpu().numpy(), axis=1)
 
             # for each detection id get the four tid prediction
             counter = {}
 
             for i, det in enumerate(buffer_detections):
-                if len(det) > 0:  # if someone detected 
+                if len(det) > 0:  # if someone detected
                     for index, row in det.iterrows():
                         tmp_box_id = row['box_id']
                         if str(tmp_box_id) not in counter:
@@ -211,22 +216,26 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
 
             # for each detection, set the processed tid
             for det in buffer_detections:
-                det['track_id'] = det['box_id'].apply(lambda x: counter[str(x)])
+                det['track_id'] = det['box_id'].apply(
+                    lambda x: counter[str(x)])
 
             del counter
             del used
 
             added = {}
-            for det in buffer_detections:
-                if len(det) > 0:  # if someone detected 
+            for i, det in enumerate(buffer_detections):
+                if len(det) > 0:  # if someone detected
                     for index, row in det.iterrows():
                         if row['track_id'] is not None:
                             if int(row['track_id']) == -1:
                                 if row['box_id'] not in added:
                                     # add fv to track_id and return last index
-                                    color = np.random.randint(0, 255, 3).tolist()
+                                    color = np.random.randint(
+                                        0, 255, 3).tolist()
+                                    fv = siamese_net.fv_encoding(Image.fromarray(crop_segmentation(
+                                        np.array(row['mask']), np.array(row['box']), buffer_frames[i]))).cpu().numpy()
                                     track_id_df = pd.concat([track_id_df, pd.DataFrame(
-                                        {"fv": [row['fv']], 'color':[color]})], ignore_index=True)
+                                        {"fv": [fv], 'color':[color]})], ignore_index=True)
                                     new_tid = track_id_df.index[-1]
                                     added[row['box_id']] = new_tid
                                     det._set_value(index, 'track_id', new_tid)
@@ -234,24 +243,31 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
                                     # print(f"aggiunto {row['box_id']}")
                                 else:
                                     det._set_value(index, 'track_id',
-                                                added[row['box_id']])
+                                                   added[row['box_id']])
                                     row['track_id'] = added[row['box_id']]
 
                             # track_id_df.loc[int(row['track_id'])]['fv'] = row['fv']
 
             del added
 
-            for f in range(frames_window//2):
-                drew_frame = buffer_frames[f].copy()
-                for indx, row in buffer_detections[f].iterrows():
+            for frame in range(frames_window//2):
+
+                drew_frame = buffer_frames[frame].copy()
+
+                for indx, row in buffer_detections[frame].iterrows():
+
                     if row['track_id'] is not None:
                         color = track_id_df.loc[int(row['track_id'])]['color']
+                        
                         drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
                             row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 3)
+
                         drew_frame = cv2.putText(drew_frame, str(row['track_id']), (int(row['box'][0]), int(
                             row['box'][1])), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 2, cv2.LINE_AA)
+
                     else:
                         color = (255, 255, 255)
+
                         drew_frame = cv2.rectangle(drew_frame, (int(row['box'][0]), int(
                             row['box'][1])), (int(row['box'][2]), int(row['box'][3])), color, 1)
 
@@ -260,11 +276,10 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
                 del drew_frame
 
         else:
-            for f in range(frames_window//2):
-                drew_frame = buffer_frames[f].copy()
+            for frame in range(frames_window//2):
+                drew_frame = buffer_frames[frame].copy()
                 video_out.write(drew_frame.copy())
                 del drew_frame
-
 
         # clean detection processing
         for det in buffer_detections:
@@ -273,10 +288,10 @@ def exec_re_id(_video_in, _video_out, _frames_window=10, total_frame=-1):
                 lambda x: x if x is not None else -1)
 
         # swap and time propagate with ciruclar buffer
-        for f_swap in range(frames_window//2):
-            buffer_frames[f_swap] = buffer_frames[f_swap +
+        for frame in range(frames_window//2):
+            buffer_frames[frame] = buffer_frames[frame +
                                                   (frames_window//2)].copy()
-            buffer_detections[f_swap] = buffer_detections[f_swap +
+            buffer_detections[frame] = buffer_detections[frame +
                                                           (frames_window//2)].copy()
 
         buffer_idx = (frames_window//2)
